082f9f74bb4f813ed91bf50cc0026ea7d5b192e1
Brayan lp <brayansystemlp@gmail.com>
2018-07-23 14:35:33 -0500

    subiendo logo de componet input lp

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+// import  InputLp  from './inputSimple';
+import InputFieldLp from './inputField';
+
+export { InputFieldLp };
+// module.exports = {
+//   InputLp,
+//   InputFieldLp,
+// };
--- src/index.js
+++ src/index.js
@@ -1,67 +1,71 @@
-// import { default as InputLp } from './components/inputSimple';
-// import { default as InputFieldLp } from './components/inputField';
+import InputLp from './components/inputSimple';
+import InputFieldLp from './components/inputField';
 
-import React from 'react';
-import PropTypes from 'prop-types';
-import styles from './components/inputField.css';
-const InputFieldLp = ({
-  // type,
-  // name,
-  label,
-  // onChange,
-  // value,
-  placeholder,
-  wrapperClass,
-  labelClass,
-  inputClass,
-  errorClass,
-  error,
-  required,
-  autoComplete,
-  disabled,
-  input,
-}) => {
-  return (
-    <div
-      className={
-        wrapperClass ? 'form-group-lp ' + wrapperClass : 'form-group-lp'
-      }
-    >
-      {label && <label className={labelClass}>{label}</label>}
-      <input
-        {...input}
-        required={required ? required : false}
-        className={
-          inputClass ? 'form-control-lp ' + inputClass : 'form-control-lp'
-        }
-        placeholder={placeholder}
-        autoComplete={autoComplete ? autoComplete : 'off'}
-        disabled={disabled ? 'true' : false}
-      />
-      {required && (
-        <span
-          className={
-            errorClass
-              ? 'invalid-feedback hide ' + errorClass
-              : 'invalid-feedback hide'
-          }
-        >
-          {error ? error : 'required field'}
-        </span>
-      )}
-    </div>
-  );
+module.exports = {
+  // InputLp,
+  InputFieldLp,
 };
+// import React from 'react';
+// import PropTypes from 'prop-types';
+// import styles from './components/inputField.css';
+// const InputFieldLp = ({
+//   // type,
+//   // name,
+//   label,
+//   // onChange,
+//   // value,
+//   placeholder,
+//   wrapperClass,
+//   labelClass,
+//   inputClass,
+//   errorClass,
+//   error,
+//   required,
+//   autoComplete,
+//   disabled,
+//   input,
+// }) => {
+//   return (
+//     <div
+//       className={
+//         wrapperClass ? 'form-group-lp ' + wrapperClass : 'form-group-lp'
+//       }
+//     >
+//       {label && <label className={labelClass}>{label}</label>}
+//       <input
+//         {...input}
+//         required={required ? required : false}
+//         className={
+//           inputClass ? 'form-control-lp ' + inputClass : 'form-control-lp'
+//         }
+//         placeholder={placeholder}
+//         autoComplete={autoComplete ? autoComplete : 'off'}
+//         disabled={disabled ? 'true' : false}
+//       />
+//       {required && (
+//         <span
+//           className={
+//             errorClass
+//               ? 'invalid-feedback hide ' + errorClass
+//               : 'invalid-feedback hide'
+//           }
+//         >
+//           {error ? error : 'required field'}
+//         </span>
+//       )}
+//     </div>
+//   );
+// };
 
-InputFieldLp.propTypes = {
-  name: PropTypes.string,
-  label: PropTypes.string.isRequired,
-  type: PropTypes.string.isRequired,
-  // onChange: PropTypes.func.isRequired,
-  placeholder: PropTypes.string,
-  autoComplete: PropTypes.string,
-  // icon: PropTypes.string,
-  error: PropTypes.string,
-};
+// InputFieldLp.propTypes = {
+//   name: PropTypes.string,
+//   label: PropTypes.string.isRequired,
+//   type: PropTypes.string.isRequired,
+//   // onChange: PropTypes.func.isRequired,
+//   placeholder: PropTypes.string,
+//   autoComplete: PropTypes.string,
+//   // icon: PropTypes.string,
+//   error: PropTypes.string,
+// };
 
-export default InputFieldLp;
+// export default InputFieldLp;
